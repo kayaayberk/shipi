024102c4fa873cfb92b24f77308f02de8b56e7da
Ayberk Kaya <kayaayberk98@gmail.com>
2024-06-07 20:51:34 +0200

    chore: add auth pages

---
+++ app/auth/signin/page.tsx
@@ -0,0 +1,5 @@
+import SigninSection from '@/views/SigninView/signin-section'
+
+export default async function SigninPage() {
+  return <SigninSection />
+}
---
+++ app/auth/verify-request/page.tsx
@@ -0,0 +1,3 @@
+export default function MagicLinkSignInPage() {
+  return <div>Check your email</div>
+}
