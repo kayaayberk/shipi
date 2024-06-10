f7f62c9a63f7ba90108272fe2e8141f946db241c
Ayberk Kaya <kayaayberk98@gmail.com>
2024-06-10 23:47:23 +0200

    chore: switch to supabase auth --next-auth adapter errors -- no support

--- app/auth/signin/page.tsx
+++
@@ -1,5 +0,0 @@
-import SigninSection from '@/views/SigninView/signin-section'
-
-export default async function SigninPage() {
-  return <SigninSection />
-}
--- app/auth/verify-request/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function MagicLinkSignInPage() {
-  return <div>Check your email</div>
-}
